22d635f168374013e8a96354d0a30541dd792a75
Hassan Mohamed <hassan.mhmd656@gmail.com>
2022-05-17 23:08:08 +0200

    sqs integration with ses

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,26 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES();
 
 async function sendMail(event, context) {
+
+  const record = event.Records[0];
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: 'hassan.mhmd656@gmail.com',
     Destination: {
-      ToAddresses: ['dx201038@yahoo.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello hsn!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   };
